9d57716a9ddbdb07319ba06f27c5dcb21d669723
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-09-02 20:11:19 +0300

    colorized

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+export GREEN='\033[0;32m'
+export NC='\033[0m' # No Color
+
+echo "Building..."
 find /src -type f -name '*.css' \
           -and -not -name '*.min.css' \
-          -exec echo Minifying {} \; \
+          -exec echo -e "      ${GREEN}process {} ${NC}" \; \
           -exec java -jar /usr/share/java/yuicompressor-2.4.8.jar --type css -o '.css$:.min.css' {} \;
 
 find /src -type f -name '*.js' \
           -and -not -name '*.min.js' \
-          -exec echo Minifying {} \; \
+          -exec echo -e "      ${GREEN}process {} ${NC}" \; \
           -exec java -jar /usr/share/java/yuicompressor-2.4.8.jar --type js -o '.js$:.min.js' {} \;
